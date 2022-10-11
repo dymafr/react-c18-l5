b833e91e9969bc6bee729c106bae215311340b0f
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-11 11:46:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c18-l5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-caqute)
---
+++ src/hooks/useFetchRecipes.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { getRecipes } from '../apis';
+
+export function useFetchRecipes(page) {
+  const [recipe, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    let cancel = false;
+    async function fetchRecipes() {
+      try {
+        setIsLoading(true);
+        const queryParam = new URLSearchParams();
+        if (page) {
+          queryParam.append('limit', 18);
+          queryParam.append('skip', (page - 1) * 18);
+          queryParam.append('sort', 'createdAt:-1');
+        }
+        const fetchedRecipes = await getRecipes(queryParam);
+        setRecipes(fetchedRecipes);
+      } catch (e) {
+        setError('Erreur');
+      } finally {
+        if (!cancel) {
+          setIsLoading(false);
+        }
+      }
+    }
+    fetchRecipes();
+    return () => (cancel = true);
+  }, [page]);
+
+  return [[recipe, setRecipes], isLoading, error];
+}
